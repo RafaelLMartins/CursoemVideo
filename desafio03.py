63cb8a55cce6c458d9660ee4b5999d66888d7f60
RafaelLMartins <rafaelluism@hotmail.com>
2022-05-10 10:38:01 -0300

    Initial commit

---
+++ desafio03.py
@@ -0,0 +1,5 @@
+print('===== DESAFIO 03 =====')
+valor1 = int (input('Primeiro número '))
+valor2 = int (input('Segundo número '))
+soma = valor1 + valor2
+print('A soma é ', soma)
---
+++ teste01.py
@@ -0,0 +1,5 @@
+nome = input('Qual seu nome?')
+idade = input('Quantos anos você tem?')
+peso = input('Qual é o seu peso?')
+print(nome, idade, peso)
+
